0a8a2e7271a7839bd5b5cca8e7de7690237bb029
OzAlbayrak <oz@albayrak.dev>
2025-02-15 22:49:37 +0100

    Add Login function

---
+++ login/index.js
@@ -0,0 +1,19 @@
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+  
+    let user = localStorage.getItem(username);
+  
+    if (user) {
+      let parsedUser = JSON.parse(user);
+      if (parsedUser.password === password) {
+        localStorage.setItem("user", JSON.stringify(parsedUser));
+        window.location.href = "/index.html";
+      } else {
+        alert("Incorrect password");
+      }
+    } else {
+      alert("User not found");
+    }
+  });  
