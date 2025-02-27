14c7d5a45a22e3b00f5c8cde8888d253552d4dd1
OzAlbayrak <oz@albayrak.dev>
2025-02-27 20:56:01 +0100

    Add todo habit and  event button

--- index.js
+++ index.js
@@ -25,19 +25,16 @@ if (user) {
   };
 
   // Aktivera senare för api anrop
-  getAdvice();
+  //getAdvice();
 
   document.getElementById("navLogoutButton").classList.remove("hidden");
   document.getElementById("navLink").classList.remove("hidden");
   document.getElementById("loginButton").classList.add("hidden");
   document.getElementById("logoutButton").classList.remove("hidden");
-  document.getElementById("addTaskButton").classList.remove("hidden");
-  document.getElementById("addEventButton").classList.remove("hidden");
   document.getElementById("addMenu").classList.remove("hidden");
-
-
-
-
+  document.getElementById("task-list-button").classList.remove("hidden");
+  document.getElementById("habit-list-button").classList.remove("hidden");
+  document.getElementById("event-list-button").classList.remove("hidden");
 
   let loggedInUser = JSON.parse(localStorage.getItem(user));
 
@@ -45,7 +42,7 @@ if (user) {
   var ul = document.createElement("ul");
   loggedInUser.tasklist.forEach((task) => {
     var li = document.createElement("li");
-    li.innerHTML = `Task: ${task}`;
+    li.innerHTML = `Task: ${task.title}`;
     ul.appendChild(li);
   });
   taskList.appendChild(ul);
@@ -68,28 +65,6 @@ if (user) {
   });
   habitList.appendChild(ul);
 
-
-
-
-
-  document.getElementById("addTaskButton").addEventListener("click", () => {
-    let userTasklistUpdate = JSON.parse(localStorage.getItem(user));
-    userTasklistUpdate.tasklist.push("Lemon");
-    localStorage.setItem(user, JSON.stringify(userTasklistUpdate));
-    alert("Add successful!");
-    window.location.href = "/index.html";
-  });
-
-  document.getElementById("addEventButton").addEventListener("click", () => {
-    let userEventListUpdate = JSON.parse(localStorage.getItem(user));
-    let name = "Test Event";
-    let start = new Date();
-    let end = new Date();
-    userEventListUpdate.eventlist.push({ id: Date.now(), name, start, end });
-    localStorage.setItem(user, JSON.stringify(userEventListUpdate));
-    alert("Add successful!");
-    window.location.href = "/index.html";
-  });
 } else {
   document.getElementById("navLoginButton").classList.remove("hidden");
   document.getElementById("loginButton").classList.remove("hidden");
@@ -114,4 +89,16 @@ document.getElementById("logoutButton").addEventListener("click", () => {
 
 document.getElementById("loginButton").addEventListener("click", () => {
   window.location.href = "/login/index.html";
+});
+
+document.getElementById("task-list-button").addEventListener("click", () => {
+  window.location.href = "/task/index.html";
+});
+
+document.getElementById("habit-list-button").addEventListener("click", () => {
+  window.location.href = "/habit/index.html";
+});
+
+document.getElementById("event-list-button").addEventListener("click", () => {
+  window.location.href = "/event/index.html";
 });
